061fe5e2d6527820bf4762602a06fa24d3806249
vito song <709465149@qq.com>
2020-05-08 10:43:45 +0800

    new project

---
+++ src/main/java/com/song/test/LearnTest.java
@@ -0,0 +1,56 @@
+package com.song.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: songgx 2020-04-29 上午11:07
+ */
+public class LearnTest {
+
+    public static void main(String[] args) {
+
+        //No.1 true
+//        float a = 0.125f;
+//        double b = 0.125d;
+//        System.out.println ((a - b) == 0.0);
+
+
+        //No.2 false
+//        double c = 0.8;
+//        double d = 0.7;
+//        double e = 0.6;
+//        System.out.println ((c -d) == (d -e));
+
+
+        //No.3 Infinity
+//        System.out.println (1.0 /0 );
+
+        //No.4 NaN
+//        System.out.println (0.0 / 0.0);
+
+//        int a = -1000 >>4;
+//        int b = -1000 >>>4;
+//        System.out.println (a);
+//        System.out.println (b);
+        String a ="";
+        switch(a){
+
+        }
+        for (int i = 0; i <100 ; i++) {
+            g(1);
+
+        }
+
+    }
+
+    private static void g(double a){
+        System.out.println (a);
+    }
+
+    private static  void g(Integer b){
+        System.out.println (b);
+    }
+
+    <String, T, Alibaba> String get(String string, T t) { return string; }
+}
